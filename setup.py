66c5ef69a9a9b147ce4a9b680f15488322098de8
Krypticdator <toni.nurmi@hotmail.com>
2016-06-26 23:53:36 +0300

    testing codeclimate addon

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FSTTRPGCharacterBasicInfo',
-    version='0.1.3',
+    version='0.1.4',
     packages=['fsttrpgbasicinfo'],
     url='',
     license='',
