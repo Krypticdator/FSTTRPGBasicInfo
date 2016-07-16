7478d69717fb71bd78b2b9fbdadda6f5fd198f2c
Krypticdator <toni.nurmi@hotmail.com>
2016-07-16 14:42:12 +0300

    updated version to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FSTTRPGCharacterBasicInfo',
-    version='0.1.6',
+    version='0.1.7',
     packages=['fsttrpgbasicinfo'],
     url='',
     license='',
