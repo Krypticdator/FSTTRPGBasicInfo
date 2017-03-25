d32b3bce6943e5d286540e166f3788c35c4a4b67
Krypticdator <toni.nurmi@hotmail.com>
2017-03-25 20:01:55 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FSTTRPGCharacterBasicInfo',
-    version='0.3.5',
+    version='0.3.6',
     packages=['fsttrpgbasicinfo', 'fsttrpgbasicinfo.traitmvc'],
     url='',
     license='',
